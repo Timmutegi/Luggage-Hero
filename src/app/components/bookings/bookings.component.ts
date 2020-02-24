872dbd1cbd392265921c1732d2c3f2ba0d2bed45
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-02-24 15:48:11 +0300

    Added flash messages

--- src/app/components/bookings/bookings.component.ts
+++ src/app/components/bookings/bookings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-bookings',
@@ -10,13 +11,16 @@ export class BookingsComponent implements OnInit {
   bookings: any;
   isLoading = true;
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
     const user = localStorage.getItem('user');
     this.api.get('/booking/customer/' + user).subscribe(
       res => {
         console.log(res);
+        if (res.length === 0) {
+          this.flashMessage.show('You have not yet booked any storage', {cssClass: 'alert-success', timeout: 10000});
+        }
         this.bookings = res;
         this.isLoading = false;
       }
