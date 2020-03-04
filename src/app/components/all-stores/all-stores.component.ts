558caf197341a50d01a6f5eab02ca2a0b1092edf
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-03-04 15:00:50 +0300

    created 'all stores' component

---
+++ src/app/components/all-stores/all-stores.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-all-stores',
+  templateUrl: './all-stores.component.html',
+  styleUrls: ['./all-stores.component.scss']
+})
+export class AllStoresComponent implements OnInit {
+  isLoading = true;
+  stores: [];
+
+  constructor(private apiService: ApiService, private router: Router) { }
+
+  ngOnInit() {
+    this.getStores();
+  }
+
+  getStores() {
+    this.apiService.get('/business').subscribe(
+      res => {
+        // console.log(res);
+        this.isLoading = false;
+        this.stores = res;
+      }
+    );
+  }
+
+  home() {
+    this.router.navigate(['home']);
+  }
+
+  book(ID: string) {
+    console.log(ID);
+    this.router.navigate([`details/${ID}`]);
+  }
+
+}
