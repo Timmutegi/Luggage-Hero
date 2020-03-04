951652c0e8305c112ec4cdf8f8037f8252daf367
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-03-04 18:23:18 +0300

    Added markers to Google map

--- src/app/components/google-map/google-map.component.ts
+++ src/app/components/google-map/google-map.component.ts
@@ -6,7 +6,7 @@ import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./google-map.component.scss']
 })
 export class GoogleMapComponent implements AfterViewInit {
-  @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
+  @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
   map: google.maps.Map;
   lng = 37.015567399999995;
   lat = -1.1069468999999998;
@@ -18,18 +18,59 @@ export class GoogleMapComponent implements AfterViewInit {
   marker = new google.maps.Marker({
     position: this.coordinates,
     map: this.map,
+    title: 'You',
+    icon: {
+      url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+    }
   });
+  markers = [
+    {
+      position: new google.maps.LatLng(-1.03326, 37.06933),
+      map: this.map,
+      title: 'Marker 2',
+    },
+    {
+      position: new google.maps.LatLng(-0.28333, 36.06667),
+      map: this.map,
+      title: 'Marker 3'
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngAfterViewInit() {
     this.mapInitializer();
   }
 
   mapInitializer() {
-    this.map = new google.maps.Map(this.gmap.nativeElement,
-    this.mapOptions);
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+
+    // ADDING CLICK EVENT TO DEFAULT MARKER
+    this.marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: this.marker.getTitle()
+      });
+      infoWindow.open(this.marker.getMap(), this.marker);
+    });
+    // ADDS DEFAULT MARKER TO MAP
     this.marker.setMap(this.map);
+
+    // ADDS OTHER MARKERS
+    this.loadAllMarkers();
   }
 
+  loadAllMarkers() {
+    this.markers.forEach(markerInfo => {
+      const marker = new google.maps.Marker({
+        ...markerInfo
+      });
+      const infoWindow = new google.maps.InfoWindow({
+        content: marker.getTitle()
+      });
+      marker.addListener('click', () => {
+        infoWindow.open(marker.getMap(), marker);
+      });
+      marker.setMap(this.map);
+    });
+  }
 }
