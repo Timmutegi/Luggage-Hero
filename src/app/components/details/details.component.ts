e8e50db6b04ffd6413a8007ccc7c66e553658113
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-03-07 18:33:01 +0300

    Changed layout and added map

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 
@@ -7,23 +7,77 @@ import { ApiService } from '../../services/api.service';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, AfterViewInit {
   ID: string;
   business: any;
   isLoading = true;
+  starList: boolean[] = [true, true, true, true, true];
+  rating: number;
+  @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
+  map: google.maps.Map;
+  lng: number;
+  lat: number;
+  coordinates: google.maps.LatLng;
+  mapOptions: google.maps.MapOptions;
+  marker: google.maps.Marker;
+  businessMarker: google.maps.Marker;
 
   constructor(private activatedRoute: ActivatedRoute, private api: ApiService, private router: Router) { }
 
   ngOnInit() {
     this.ID = this.activatedRoute.snapshot.params.ID;
-    console.log(this.ID);
     this.api.get('/business/' + this.ID).subscribe(
       res => {
-        // console.log(res);
         this.isLoading = false;
         this.business = res;
       }
     );
+  }
+
+  ngAfterViewInit() {
+    this.api.get('/business/' + this.ID).subscribe(res => {
+      console.log(this.business);
+      this.coordinates = new google.maps.LatLng(
+        res.latitude,
+        res.longitude
+      );
+      this.mapOptions = {
+        center: this.coordinates,
+        zoom: 10
+      };
+      this.marker = new google.maps.Marker({
+        position: this.coordinates,
+        map: this.map,
+        title: this.business.name
+      });
+
+      this.mapInitializer();
+    });
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+
+    // ADDING CLICK EVENT TO DEFAULT MARKER
+    this.marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: this.marker.getTitle()
+      });
+      infoWindow.open(this.marker.getMap(), this.marker);
+    });
+    // ADDS DEFAULT MARKER TO MAP
+    this.marker.setMap(this.map);
+  }
+
+  setRating(data: any) {
+    this.rating = data + 1;
+    for (let i = 0; i <= 4; i++) {
+      if (i <= data) {
+        this.starList[i] = false;
+      } else {
+        this.starList[i] = true;
+      }
+    }
   }
 
   confirm(ID: string) {
