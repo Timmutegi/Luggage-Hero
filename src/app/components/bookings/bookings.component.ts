9042efc1411d3d036f0b9a9cb34cf85d90e406d5
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-03-10 13:15:33 +0300

    Added duration of booking

--- src/app/components/bookings/bookings.component.ts
+++ src/app/components/bookings/bookings.component.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 export class BookingsComponent implements OnInit {
   bookings: any;
   isLoading = true;
+  duration: boolean;
 
   constructor(private api: ApiService, private flashMessage: FlashMessagesService, private router: Router) { }
 
@@ -18,7 +19,6 @@ export class BookingsComponent implements OnInit {
     const user = localStorage.getItem('user');
     this.api.get('/booking/customer/' + user).subscribe(
       res => {
-        console.log(res);
         if (res.length === 0) {
           this.flashMessage.show('You have not yet booked any storage', {cssClass: 'alert-success', timeout: 10000});
         }
@@ -27,6 +27,27 @@ export class BookingsComponent implements OnInit {
         this.bookings.forEach((booking: any) => {
           booking.shop.longitude = parseFloat(booking.shop.longitude);
           booking.shop.latitude = parseFloat(booking.shop.latitude);
+          // CALCULATES DURATION OF BOOKING
+          const current = new Date ();
+          const checkIn = new Date (booking.check_in);
+          let difference = Math.abs(current.getTime() - checkIn.getTime()) / 1000;
+
+          if (difference > 0) {
+             this.duration = true;
+             const days = Math.floor(difference / 86400);
+             difference -= days * 86400;
+
+             const hours = Math.floor(difference / 3600) % 24;
+             difference -= hours * 3600;
+
+             const minutes = Math.floor(difference / 60) % 60;
+             difference -= minutes * 60;
+             const seconds = Math.round(difference);
+
+             booking.duration = `${days} Days ${hours} Hours ${minutes} Minutes ${seconds} seconds`;
+          }
+
+          this.duration = false;
         });
       }
     );
