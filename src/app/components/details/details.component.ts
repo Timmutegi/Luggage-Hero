4687530e8e23cbed8a0a0d6cf64663905816328f
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-03-16 11:25:26 +0300

    sets the status of new booking

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -85,7 +85,7 @@ export class DetailsComponent implements OnInit, AfterViewInit {
       {
         customer: user,
         shop: ID,
-        status: 'pending'
+        status: 'Pending'
       }
     ));
     this.api.post('/booking/create', booking).subscribe(
