6b1fda210e4565f120e07a26dd209085256b5091
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-04-01 14:09:49 +0300

    animations file

--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -1,26 +1,36 @@
 import { trigger, transition, style, query, animateChild, animate, group } from '@angular/animations';
 
-export const fadeInAnimation = trigger('routeAnimations', [
+export const slideInAnimation = trigger('routeAnimations', [
   transition('* <=> *', [
     style({ position: 'relative' }),
-    query(':enter', [
+    query(':enter, :leave', [
       style({
-        opacity: 0,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%'
       })
-    ],
-    {
-        optional: true
-    }),
-     query(
-      ':leave',
-       [style({ opacity: 1 }), animate('0.3s', style({ opacity: 0 }))],
-      { optional: true }
-    ),
-    query(
-      ':enter',
-      [style({ opacity: 0 }), animate('0.3s', style({ opacity: 1 }))],
-      { optional: true }
-    )
+    ]),
+    query(':enter', [style({ left: '-100%' })]),
+    query(':leave', animateChild()),
+    group([
+      query(':leave', [animate('300ms ease-out', style({ left: '100%' }))]),
+      query(':enter', [animate('300ms ease-out', style({ left: '0%' }))])
+    ]),
+    query(':enter', animateChild())
   ]),
 ]);
+
+export const fadeInAnimation =
+  // trigger name for attaching this animation to an element using the [@triggerName] syntax
+  trigger('Animations', [
+    // route 'enter' transition
+    transition('* <=> *', [
+      // css styles at start of transition
+      style({ opacity: 0 }),
+
+      // animation and styles at end of transition
+      animate('20s', style({ opacity: 1 }))
+    ])
+  ]);
 
