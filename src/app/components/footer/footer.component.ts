47aa0bd20d766ee35614fa0e62df388d27435031
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-04-10 15:20:14 +0300

    Added form and form validation to footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  subscribeForm: FormGroup;
+  submitted: boolean;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.subscribeForm = this.formBuilder.group({
+      subscribe: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,5}$')]]
+    });
+  }
+
+  get f() {
+    return this.subscribeForm.controls;
+  }
+
+  subscribe() {
+    this.submitted = true;
+    if (this.subscribeForm.invalid) {
+      return;
+    }
+    const email = this.subscribeForm.value.subscribe;
+    console.log(email);
+
   }
 
 }
