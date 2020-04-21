4445968066ef901465f6c9c992de8cba7b5048bd
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-04-21 13:19:42 +0300

    Improved layout and validation

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -26,7 +26,7 @@ export class SignUpComponent implements OnInit {
       lastname: ['', Validators.required],
       email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,5}$')]],
       phone: ['', Validators.required],
-      password: ['', Validators.required]
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
