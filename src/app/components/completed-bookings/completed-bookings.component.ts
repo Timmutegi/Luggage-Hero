067b5e71eb236e230c252f0afc7cf284130f9b68
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-05-27 14:10:03 +0300

    integrated API

--- src/app/components/completed-bookings/completed-bookings.component.ts
+++ src/app/components/completed-bookings/completed-bookings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-completed-bookings',
@@ -6,10 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./completed-bookings.component.scss']
 })
 export class CompletedBookingsComponent implements OnInit {
+  pendingBookings: any;
+  isLoading = true;
+  directions: any = [];
+  lat: number;
+  lng: number;
+  message: string;
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.lng = position.coords.longitude;
+      this.lat = position.coords.latitude;
+    });
+
+    this.getBookings();
+  }
+
+  getBookings() {
+    const user = localStorage.getItem('user');
+    this.api.get('/booking/customer/' + user).subscribe(
+      res => {
+        if (res === undefined || res.length === 0) {
+          this.message = 'You have not booked yet';
+        }
+        res.forEach((booking: any) => {
+          booking.shop.longitude = parseFloat(booking.shop.longitude);
+          booking.shop.latitude = parseFloat(booking.shop.latitude);
+        });
+        this.pendingBookings = res;
+        this.isLoading = false;
+      },
+    );
+  }
+
+  getDirections(latitude: number, longitude: number) {
+    const directions = {
+      origin: {
+        lng: this.lng,
+        lat: this.lat,
+      },
+      destination: {
+        lng: longitude,
+        lat: latitude,
+      },
+    };
+    this.directions.push(directions);
   }
 
 }
