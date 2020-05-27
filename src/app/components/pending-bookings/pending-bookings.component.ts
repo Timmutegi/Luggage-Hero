d9c3179c250fe3fa4f000424ef7ffa87ba4a86be
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-05-27 12:07:17 +0300

    created pending bookings component

---
+++ src/app/components/pending-bookings/pending-bookings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pending-bookings',
+  templateUrl: './pending-bookings.component.html',
+  styleUrls: ['./pending-bookings.component.scss']
+})
+export class PendingBookingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
