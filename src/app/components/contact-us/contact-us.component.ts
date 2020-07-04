e12b099c8932a0ba48b4d967df94dbe8d149188e
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-07-04 18:59:57 +0300

    Added contact us form, styling and logic

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
+  contactForm: FormGroup;
+  submitted = false;
+  isLoading: boolean;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,5}$')]],
+      message: ['', Validators.required]
+    });
+  }
+
+  get f() {
+    return this.contactForm.controls;
+  }
+
+  submit() {
+
   }
 
 }
